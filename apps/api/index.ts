2a2d013d871f4066249dbb9568645521a96dc611
Keshav Garg <keshav2003garg@gmail.com>
2025-06-19 14:11:44 +0530

    fix: update server port configuration to use environment variable

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -64,8 +64,8 @@ app.use('/api/export', exportRoute);
 
 app.use(errorMiddleware);
 
-app.listen(5000, () => {
-  console.log(`Server is running on port ${5000}`);
+app.listen(process.env.PORT || 80, () => {
+  console.log(`Server is running on port ${process.env.PORT || 80}`);
 });
 
 export const handler = serverlessExpress({ app });
