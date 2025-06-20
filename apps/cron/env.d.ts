6918b6b056858c2c5c4638bac7b286d3f237a022
Keshav Garg <keshav2003garg@gmail.com>
2025-06-20 22:55:15 +0530

    feat: add SMTP environment variables and update cron function configuration

--- apps/cron/env.d.ts
+++ apps/cron/env.d.ts
@@ -3,6 +3,13 @@ declare module 'global' {
     namespace NodeJS {
       interface ProcessEnv {
         MONGO_URI: string;
+        SMTP_NAME: string;
+        SMTP_MAIL: string;
+        SMTP_REPLY_TO: string;
+        SMTP_HOST: string;
+        SMTP_PORT: string;
+        SMTP_USERNAME: string;
+        SMTP_PASSWORD: string;
       }
     }
   }
