6e53997b418b23e2d8d27adf14fb444717f52f8a
shahzan01 <m.shahzan2003@gmail.com>
2025-06-24 10:22:59 +0530

    feat : add inventory edit and delete

---
+++ apps/admin/src/components/dashboard/inventory/edit-inventory-dialog/edit-inventory-dialog.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+// Node Modules
+import React, { memo } from 'react';
+
+// Components
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@repo/ui/components/base/dialog';
+import EditInventoryForm from './edit-inventory-form';
+
+// Types
+import { InventoryResponse } from '@/hooks/useInventory/types';
+
+interface EditInventoryDialogProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  inventoryItem: InventoryResponse;
+}
+
+function EditInventoryDialog(props: EditInventoryDialogProps) {
+  const { open, setOpen, inventoryItem } = props;
+
+  function handleSuccess() {
+    setOpen(false);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="max-w-md">
+        <DialogHeader className="sr-only">
+          <DialogTitle>Rediger Lager</DialogTitle>
+        </DialogHeader>
+        <EditInventoryForm
+          inventoryItem={inventoryItem}
+          onSuccess={handleSuccess}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default memo(EditInventoryDialog);
